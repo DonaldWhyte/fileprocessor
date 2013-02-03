140204ad2069bbacb1c164e7a7a0bcaa6024f324
Donald Whyte <donaldwhyte0@gmail.com>
2013-02-03 21:20:13 +0000

    Finished implementing built-in filters.

--- fileprocessor/filterers.py
+++ fileprocessor/filterers.py
@@ -1,11 +1,99 @@
 """Contains all built-in Filterer classes."""
 
+import collections
+import fnmatch
+import os
+
 from fileprocessor.abstracts import Filterer
+
 
 class ExcludeListFilterer(Filterer):
 
-	pass
+	"""Filterer which filters files based on glob patterns."""
+
+	def __init__(self, excludeList):
+		"""Construct instance of ExcludeListFilterer.
+
+		Arguments:
+		excludeList -- List of glob patterns which will be used as
+					   a a black list to remove files from the listing.
+
+		"""
+		if not isinstance(excludeList, collections.Iterable):
+			raise TypeError("Exclusion list should be an iterable collection of strings")
+		self.excludeList = excludeList
+
+	def filter(self, fileListing):
+		"""Filter file listing based on stored glob patterns.
+
+		Returns NEW list containing the the files which passed the filter.
+
+		Arguments:
+		fileListing -- A list containing the absolute paths of the
+					   files to filter.
+
+		"""
+		if not isinstance(fileListing, collections.Iterable):
+			raise TypeError("List of files to filter should be an iterable collection of strings")
+		newListing = list(fileListing)
+
+		# Go through each entry, checking if they match one of the
+		# glob patterns. If an entry does, it is removed
+		i = 0
+		while i < len(newListing):
+			filtered = False
+			for exclusion in self.excludeList:
+				if fnmatch.fnmatch(newListing[i], exclusion):
+					filtered = True
+					del newListing[i]
+					break
+			# If the current element was not deleted, increment
+			# the index into the file listing. We don't do it if
+			# an element was deleted as the next element would
+			# have moved ot the delete elements's index
+			if not filtered:
+				i += 1
+
+		return newListing
+
 
 class ExtensionFilterer(Filterer):
 
-	pass
+	def __init__(self, allowedExtensions):
+		"""Construct instance of ExtensionFilterer.
+
+		Arguments:
+		allowedExtensions -- List of allowed extensions (e.g. ["txt", "py"]).
+							 Any files which don't have these extensions
+							 will be removed.
+
+		"""
+		if not isinstance(allowedExtensions, collections.Iterable):
+			raise TypeError("Allowed extension list should be an iterable collection of strings")
+		self.allowedExtensions = allowedExtensions
+
+	def filter(self, fileListing):
+		"""Filter file listing based on stored extension whitelist.
+
+		Returns NEW list containing the the files which passed the filter.
+
+		Arguments:
+		fileListing -- A list containing the absolute paths of the
+					   files to filter.
+
+		"""
+		if not isinstance(fileListing, collections.Iterable):
+			raise TypeError("List of files to filter should be an iterable collection of strings")
+		newListing = list(fileListing)
+
+		i = 0
+		while i < len(newListing):
+			name, extension = os.path.splitext(newListing[i])
+			# Increment index and allow file if its extension is in
+			# the white list
+			if extension[1:] in self.allowedExtensions:
+				i += 1
+			else:
+				del newListing[i]
+
+		return newListing
--- tests/test_filterers.py
+++ tests/test_filterers.py
@@ -4,3 +4,96 @@ import os
 sys.path.append(os.environ.get("PROJECT_ROOT_DIRECTORY", "."))
 
 from fileprocessor.filterers import *
+
+class TestExcludeListFilterer(unittest.TestCase):
+
+	def setUp(self):
+		self.originalFileListing = [
+			# Stuff that should be filtered
+			"/exact_filename_path.txt",
+			"/root/hello/something.txt", "/root/hello/other.txt",
+			"test_fileprocessor.py",
+			"/root/hello.txt",
+			# Stuff that should be allowed
+			"/root/test_something.py", 
+			"/root/byebye.txt~",
+			"/root/stuff/five.cpp",
+			"/root/toto/test.db"
+		]
+		self.excludeList = [
+			"/exact_filename_path.txt", # exact path
+			"/root/hello/*", # wildcard at end
+			"test_*.py", # wildcard in middle
+			"*.txt", # wildcard at the beginning
+		]
+		self.emptyListFilterer = ExcludeListFilterer([])
+		self.listFilterer = ExcludeListFilterer(self.excludeList)
+
+	def tearDown(self):
+		self.originalFileListing = None
+		self.excludeList = None
+		self.emptyListFilterer = None
+		self.listFilterer = None
+
+	def test_construction(self):
+		# Test invalid type
+		with self.assertRaises(TypeError):
+			ExcludeListFilterer(4543)
+		# Test properties were assigned correctly
+		self.assertEqual(self.emptyListFilterer.excludeList, [])
+		self.assertEqual(self.listFilterer.excludeList, self.excludeList)
+
+	def test_filter(self):
+		FILTERED_LISTING = [ 
+			"/root/test_something.py",  "/root/byebye.txt~",
+			"/root/stuff/five.cpp", "/root/toto/test.db"
+		]
+
+		# Test with invalid type
+		with self.assertRaises(TypeError):
+			self.emptyListFilterer.filter(543)
+		# Test different filterers
+		self.assertEqual(self.emptyListFilterer.filter(self.originalFileListing),
+			self.originalFileListing)
+		self.assertEqual(self.listFilterer.filter(self.originalFileListing),
+			FILTERED_LISTING)
+
+class TestExtensionFilterer(unittest.TestCase):
+
+	def setUp(self):
+		self.originalFileListing = [
+			# Stuff that shouldn't be allowed
+			"hello.txt~", "stuff.bin", "binary file",
+			"Huh?", "C:/something.win32", "/var/lib/python3.3",
+			".hgignore",
+			# Stuff that should be allowed
+			"test_something.py", "haha.py.py",
+			"/var/lib/python/LICENSE.txt"
+			".txt", "final.txt"
+		]
+		self.emptyExtensionFilterer = ExtensionFilterer([])
+		self.extensionFilterer = ExtensionFilterer(["py", "txt"])
+
+	def tearDown(self):
+		self.originalFileListing = None
+		self.emptyExtensionFilterer = None
+		self.extensionFilterer = None
+
+	def test_construction(self):
+		# Test invalid type for extension
+		with self.assertRaises(TypeError):
+			ExtensionFilterer(44)
+		# Test stub filterers was constructed correctly
+		self.assertEqual(self.emptyExtensionFilterer.allowedExtensions, [])
+		self.assertEqual(self.extensionFilterer.allowedExtensions, ["py", "txt"])
+
+	def test_filter(self):
+		FILTERED_LISTING = [ 
+			"test_something.py", "haha.py.py",
+			"/var/lib/python/LICENSE.txt"
+			".txt", "final.txt"
+		]
+		# Test having no extensions allowed
+		self.assertEqual(self.emptyExtensionFilterer.filter(self.originalFileListing), [])
+		# Test having some extensions allowed
+		self.assertEqual(self.extensionFilterer.filter(self.originalFileListing), FILTERED_LISTING)
